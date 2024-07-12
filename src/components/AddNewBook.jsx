3f7ad0ee3ccf268329cb4c87b8a7cc1bb6edf058
Muxitdin <toraananas@gmail.com>
2024-07-12 14:47:53 +0500

    shadcn ui conneccted/author and category select tags added

--- src/components/AddNewBook.jsx
+++ src/components/AddNewBook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import s from "../pages/styles/AddNewBook.module.css"
 
@@ -13,6 +13,25 @@ export default function AddNewBook( {setAddBookModal} ) {
         image: ""
     })
 
+    const [authors, setAuthors] = useState([])
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        const fetchAllAuthors = async () => {
+            const { data } = await axios.get(`http://localhost:3000/api/auth/allusers`)
+            setAuthors(data)
+            console.log(authors);
+        }
+        const fetchAllCategories = async () => {
+            const { data } = await axios.get(`http://localhost:3000/api/categories`)
+            setCategories(data)
+            console.log(categories);
+        }
+
+        fetchAllAuthors();
+        fetchAllCategories();
+    }, [])
+
     const getInputValue = (e) => {
         setNewBook({
             ...newBook,
@@ -22,9 +41,13 @@ export default function AddNewBook( {setAddBookModal} ) {
 
     const handleAddNewBook = async (e) => {
         e.preventDefault()
-        console.log(newBook)
-        await axios.post("http://localhost:3000/api/books/", newBook)
-        setAddBookModal(false)
+        try {
+            console.log(newBook)
+            await axios.post("http://localhost:3000/api/books", newBook)
+            setAddBookModal(false)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
@@ -32,13 +55,38 @@ export default function AddNewBook( {setAddBookModal} ) {
             <form onSubmit={(e) => handleAddNewBook(e)}>
                 <button onClick={() => setAddBookModal(false)} type="button" class="btn-close" aria-label="Close"></button>
                 <h1>Add New Book</h1>
-                <input onChange={(e) => getInputValue(e)} name='name' type="text" placeholder='Name of book' />
-                <input onChange={(e) => getInputValue(e)} name='year' type="text" placeholder='Year' />
-                <input onChange={(e) => getInputValue(e)} name='author' type="text" placeholder='Author' />
-                <input onChange={(e) => getInputValue(e)} name='description' type="text" placeholder='Description' />
-                <input onChange={(e) => getInputValue(e)} name='image' type="text" placeholder='Image URL' />
-                <input onChange={(e) => getInputValue(e)} name='price' type="text" placeholder='Price' />
-                <input onChange={(e) => getInputValue(e)} name='category' type="text" placeholder='Genre' />
+                <input onChange={getInputValue} name='name' type="text" placeholder='Name of book' />
+                <input onChange={getInputValue} name='year' type="text" placeholder='Year' />
+                {/* <Select onValueChange={handleSelectChange} name="author">
+                    <SelectTrigger>
+                        <SelectValue placeholder="select author" value="" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Authors</SelectLabel>
+                            {authors?.map((author) => (
+                                <SelectItem key={author?._id} value={author?._id}>{author?.fullName}</SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select> */}
+                <input onChange={getInputValue} name='description' type="text" placeholder='Description' />
+                <input onChange={getInputValue} name='image' type="text" placeholder='Image URL' />
+                <input onChange={getInputValue} name='price' type="text" placeholder='Price' />
+                {/* <input onChange={getInputValue} name='category' type="text" placeholder='Genre' /> */}
+                {/* <input onChange={getInputValue} name='author' type="text" placeholder='Author' /> */}
+                <select onChange={getInputValue} name="author" className='p-[0.5rem]'>
+                    <option value="">Select Author</option>
+                    {authors?.map((author) => (
+                        <option key={author?._id} value={author?._id}>{author?.fullName}</option>
+                    ))}
+                </select>
+                <select onChange={getInputValue} name="category" className='p-[0.5rem]'>
+                    <option value="">Select Category</option>
+                    {categories?.map((cat) => (
+                        <option key={cat?._id} value={cat?.name}>{cat?.name}</option>
+                    ))}
+                </select>
                 <button type="submit" class="btn btn-success">Create</button>
             </form>
         </div>
